1a516923c3a16a3a9264fdf55a0c951a9e8ff4eb
Onrwir <115116487+Onrwir@users.noreply.github.com>
2023-12-26 02:46:41 +0100

    Created bullet item layout and Recycler view

---
+++ app/src/main/java/pl/onrwir/strzelnica/Bullet.kt
@@ -0,0 +1,5 @@
+package pl.onrwir.strzelnica
+
+import java.net.URI
+
+data class Bullet(val name:String, val cost:Double, val imageURI: URI)
---
+++ app/src/main/java/pl/onrwir/strzelnica/MainActivity.kt
@@ -0,0 +1,11 @@
+package pl.onrwir.strzelnica
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
